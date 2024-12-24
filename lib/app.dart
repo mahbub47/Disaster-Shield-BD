356f985fa8001eb1a0f2c6fb321f0bd8e8f088e6
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2024-12-25 03:43:48 +0600

    splash screen implemented with await functionality

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,26 @@
 import 'package:disaster_shield_bd/utils/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'utils/themes/custom_themes/text_theme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initialize();
+  }
+  void initialize() async {
+    await Future.delayed(const Duration(seconds: 3));
+    FlutterNativeSplash.remove();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
