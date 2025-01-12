59c622b398af08ddbbbdca6f97913091c56ab64b
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2025-01-13 00:45:42 +0600

    on the way to complete onboarding screen

---
+++ lib/utils/constants/images.dart
@@ -0,0 +1,11 @@
+class Dimages {
+  Dimages._();
+
+  static const String onboardingImage01 = "assets/images/onboarding01.png";
+  static const String onboardingImage02 = "assets/images/onboarding02.png";
+  static const String onboardingImage03 = "assets/images/onboarding03.png";
+  static const String onboardingImage04 = "assets/images/onboarding04.png";
+
+  static const String mainLogo = "assets/logo/logo.png";
+
+}
